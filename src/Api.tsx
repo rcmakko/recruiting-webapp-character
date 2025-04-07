8341ae4adc2fc08a628e894991cec8c30ba549f0
rcmakko <rachel.c.mak@gmail.com>
2025-04-07 15:48:31 -0400

    proposed changes for program from interview

--- src/Api.tsx
+++ src/Api.tsx
@@ -1,3 +1,8 @@
 export function getModifier(attributeNum: number): number {
-  return Math.floor((attributeNum - 10) / 2);
+  return Math.floor((attributeNum - 10) / 3);
+}
+
+export function setUserTimeout(mostRecentTimeoutID: number): number{
+    const num = setTimeout(...)
+    return num
 }
